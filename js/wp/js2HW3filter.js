5fb05112cc0e2cd5e973398088452983346f5853
ChadChenYu <201058388chen@gmail.com>
2024-03-22 11:58:20 +0800

    add jsHW23

---
+++ js/wp/js2HW3filter.js
@@ -0,0 +1,21 @@
+function filter(a, f)
+{
+    var result = []
+    for(var i = 0;i < a.length;i++)
+    {
+        if(f(a[i]))
+        {
+            result.push(a[i])
+        }
+    }
+    return result
+
+}
+
+var array=[1,2,3,4]
+var filteredArray = filter(array, function(x)
+{
+    return x % 2 == 1;
+})
+
+console.log(filteredArray)
