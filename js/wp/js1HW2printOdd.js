e6653b592db771f63f26e9accaed33051d59b44a
ChadChenYu <201058388chen@gmail.com>
2024-03-22 10:46:58 +0800

    add js2HW1min.js

--- js/wp/js1HW2printOdd.js
+++ js/wp/js1HW2printOdd.js
@@ -1,5 +1,6 @@
 function printOddNumbers(n) {
-    for (var i = 1; i < n; i += 2) {
+    for (var i = 1; i < n; i += 2) 
+    {
         console.log(i);
     }
 }
---
+++ js/wp/js2HW1min.js
@@ -0,0 +1,17 @@
+function min(m,n)
+{
+    if(m > n)
+    {
+        return n
+    } else if(n > m)
+    {
+        return m
+    } else if(m == n)
+    {
+       return "The number is equal"
+    }
+    
+}
+
+var result = min(20, 20);
+console.log(result);
